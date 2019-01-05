83a3d68b63470d6d0913c91aad380809917ba731
lova233 <andrealovati.cube3@gmail.com>
2019-01-05 12:19:44 +0000

    adjust alignment in gameover state

--- js/game/states/gameover.js
+++ js/game/states/gameover.js
@@ -1,18 +1,16 @@
 AcquaRush.GameOver = {
     create: function() {
         console.log(game,"il giuco")
-        let style = { font: "2rem Roboto", fill: "#FFFFFF", align: "center" };
+        let style = { font: "3rem Roboto", fill: "#FFFFFF", align: "center" };
         this.background = game.add.tileSprite(0, 0, game.width, game.height - 1, 'background');
         this.background.autoScroll(-30, -0);
-        this.again = game.add.sprite(500, 245, 'again');
+        this.again = game.add.sprite(670, 260, 'again');
 	    game.add.tween(this.again).from( { y: -200 }, 2000, Phaser.Easing.Bounce.Out, true);
-        this.menu = game.add.sprite(270, 245, 'menu');
+        this.menu = game.add.sprite(370, 260, 'menu');
         game.add.tween(this.menu).from( { y: -200 }, 2000, Phaser.Easing.Bounce.Out, true);    
-        this.scoreDistance = game.add.text(370, 100, 'DISTANCE:' + game.scores.distance , style);
-        
+        this.scoreDistance = game.add.text(440, 150, 'BEST DISTANCE:' + game.scores.distance , style);    
         this.menu.inputEnabled = true;
         this.menu.events.onInputDown.add(this.menuStart, this);
-                
         this.again.inputEnabled = true;
         this.again.events.onInputDown.add(this.newGame, this);
     
